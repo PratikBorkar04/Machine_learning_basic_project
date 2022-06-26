e0757343f4a25792ce46aa57b0bd3306fc6765bc
Pratik Borkar <pratikab01@gmail.com>
2022-06-27 00:42:14 +0530

    Exception file updated

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def index():
         raise Exception ("We are generating custom exception")
 
     except Exception as ex:
-        raise HousingException(e,sys)
+        housing = HousingException(ex,sys)
         logging.info(housing.error_message)
-        logging.info("PAsing logging module")
+        logging.info("Pasing logging module")
     return "First Heroku Deplyomnet"
 
 if __name__ == "__main__":
